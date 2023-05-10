818085c783536495bbe8bcbb0c7b54961304d61a
mrizwanashiq <mrizwanashiq@outlook.com>
2023-05-10 16:57:24 +0500

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ArticlesModule } from './modules/articles/articles.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    ArticlesModule,
+  ]
+})
+export class AppModule {}
