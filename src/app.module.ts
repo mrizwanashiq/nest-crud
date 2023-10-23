ff433b1c6daeec08013643a78f3ee9cf8f482d35
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2023-10-23 23:58:44 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ArticlesModule } from './modules/articles/articles.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-crud'),
     ArticlesModule,
   ]
 })
